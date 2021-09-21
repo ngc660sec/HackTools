00113bf8716a7f0239757b36e86032e7544f0066
Se37-山屿 <52269948+Se37@users.noreply.github.com>
2021-09-21 17:49:03 +0800

    Update and rename Sqlmap批量扫描/Sqlmap_Batch_scan.py to Sqlmap 绘图扫描/Sqlmap_Batch_scan.py

--- Sqlmap 绘图扫描/Sqlmap_Batch_scan.py
+++ Sqlmap 绘图扫描/Sqlmap_Batch_scan.py
@@ -1,4 +1,4 @@
-import os
+importimport os
 from concurrent.futures import ThreadPoolExecutor
 import hashlib
 
@@ -13,9 +13,10 @@ class Sqlmap_batch_scan():
         #存放报告的路径
         report_path = 'E:/python/HACK/Sqlmap批量扫描/'
         os_command = r'sqlmap.py -u "{}" --batch > "{}{}.txt"'.format(sqlmap_url,report_path,new_url_name)
-        # print(os_command)
         os.system(os_command)
         print(f'[+]>>>已生成报告:{new_url_name}.txt')
+        fp = open('./'+new_url_name+'.txt','a+')
+        fp.write('\n'+'测试url:'+sqlmap_url+' \n')
 
     def run(self,sqlmap_url,url_name):
         self.run_sqlmap(sqlmap_url,url_name)
@@ -23,7 +24,7 @@ class Sqlmap_batch_scan():
 if __name__ == '__main__':
     sqlmap_batch_scan = Sqlmap_batch_scan()
     #线程池数量
-    sqlmap_pool = ThreadPoolExecutor(10)
+    sqlmap_pool = ThreadPoolExecutor(1)
     sqlmap_file = open('./sqlmap_url.txt','r')
     sqlmap_urls = sqlmap_file.readlines()
     for url in sqlmap_urls:
